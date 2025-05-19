7c103b91e84295c6accd5cc947722960e613f56b
Renanzio <155386642+Renanzio@users.noreply.github.com>
2025-05-19 00:56:14 -0300

    Add files via upload

--- Atividade/login.js
+++ Atividade/login.js
@@ -1,3 +1,5 @@
+const { useId } = require("react")
+
 function logar() {
     var Email = document.querySelector("#email").value
     var Password = document.querySelector("#senha").value
@@ -25,4 +27,40 @@ function usersenhas(){
     form.addEventListener("submit", function(e){
     e.preventDefault()
 })
+}
+
+function TemaEscuro(){
+    document.getElementById("Tema").innerText = "Tema Claro";
+    document.querySelector("body").style.backgroundColor = "rgb(14, 13, 13)";
+    document.querySelector("main").style.backgroundColor ="#1a1918";
+    document.querySelector("header").style.backgroundColor = "#0c1305";
+    document.getElementById("navlist1").style.backgroundColor = "#0c1305";
+    document.getElementById("navlist2").style.backgroundColor = "#0c1305";
+    document.getElementById("navlist3").style.backgroundColor = "#0c1305";
+    document.getElementById("navlist4").style.backgroundColor = "#0c1305";
+    document.querySelector("footer").style.backgroundColor = "#470b0b";
+    document.getElementById("divi1").style.backgroundColor = "#0d1407";
+    document.getElementById("divi2").style.backgroundColor = "#0d1407";
+    document.getElementById("divi3").style.backgroundColor = "#0d1407";
+    document.getElementById("divi1").style.borderColor = "#0d1407";
+    document.getElementById("divi2").style.borderColor = "#0d1407";
+    document.getElementById("divi3").style.borderColor = "#0d1407";
+    document.getElementById("divnav").style.backgroundColor = "rgba(14, 13, 19, 0.9)";
+}
+function TemaClaro() {
+    document.getElementById("Tema").innerText = "Tema Escuro";
+    document.querySelector("body").style.backgroundColor ="#ffffff";
+    document.querySelector("main").style.backgroundColor ="#383838";
+    document.querySelector("header").style.backgroundColor = "#2a4116";
+    document.getElementById("navlist1").style.backgroundColor = "#2a4116";
+    document.getElementById("navlist2").style.backgroundColor = "#2a4116";
+    document.getElementById("navlist3").style.backgroundColor = "#2a4116";
+    document.getElementById("navlist4").style.backgroundColor = "#2a4116";
+    document.querySelector("footer").style.backgroundColor = "#a30c0c";
+    document.getElementById("divi1").style.backgroundColor = "#2a4116";
+    document.getElementById("divi2").style.backgroundColor = "#2a4116";
+    document.getElementById("divi3").style.backgroundColor = "#2a4116";
+    document.getElementById("divi1").style.borderColor = "#ffffff";
+    document.getElementById("divi2").style.borderColor = "#ffffff";
+    document.getElementById("divi3").style.borderColor = "#ffffff";
 }
